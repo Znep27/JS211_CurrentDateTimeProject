a7a0f22700ac3444285c130c0328b31aadc3d8a6
Marc Rozenburgh <znep27@hotmail.com>
2023-06-22 18:18:48 -0500

    pushing updates

--- main.js
+++ main.js
@@ -15,11 +15,17 @@ const displayDate = () => {
  
 
 // Write a JavaScript program to convert a number to a string.
-
+const numberToString = (num) => {
+  let text = num.toString();
+  return text;
+}
 
 
 // Write a JavaScript program to convert a string to the number.
-
+const stringToNumber = (string1) => {
+  let number = parseInt(string1);
+  return number;
+}
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
@@ -29,23 +35,76 @@ const displayDate = () => {
   // * Number
   // * NaN
   // * String
-  
+  const whatDateType = (entry) => {
+    console.log(typeof entry);
+  }
 
   
 // Write a JavaScript program that adds 2 numbers together.
+const add1 = (number1, number2) => {
+  let answer = number1 + number2;
+  return answer;
+}
 
+const addNum = document.getElementById('calculateNumbers');
+addNum.addEventListener("submit", e => {
+  e.preventDefault()
+  const num1 = document.getElementById("first-Number").value;
+  const num2 = document.getElementById("second-Number").value;
+  const sum1 = add1(stringToNumber(num1), stringToNumber(num2));
+  document.getElementById("sum").innerHTML = `The sum of ${num1} + ${num2} is ${sum1}.<br><br>`;
+});
+  
+const thingCheck = document.getElementById('checkThings');
+thingCheck.addEventListener("submit", e => {
+  e.preventDefault()
+  let thing1 = document.getElementById("first-Value").value;
+  let thing2 = document.getElementById("second-Value").value;
+  if (!isNaN(thing1)) {
+    thing1 = stringToNumber(thing1)
+  }
+  if (!isNaN(thing2)) {
+    thing2 = stringToNumber(thing2)
+  }
+  console.log(typeof thing1 + typeof thing2)
+  document.getElementById("checked").innerHTML = `<br>${thing1} is a ${typeof thing1}, ${thing2} is a ${typeof thing2}.`;
+  bothTrue(thing1, thing2)
+  neitherTrue(thing1, thing2)
+});
 
 
 // Write a JavaScript program that runs only when 2 things are true.
-
+const bothTrue = (thing1, thing2) => {
+  if (thing1 && thing2) {
+    console.log(thing1 + ' and ' + thing2 + ' are both true');
+    document.getElementById("trueOrFalse").innerHTML = `${thing1} and ${thing2} are both truthy.`;
+  } else {
+    oneTrue(thing1, thing2)
+  }
+}
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
-
+const oneTrue = (thing1, thing2) => {
+  if (thing1 || thing2) {
+    if(thing1) {
+      console.log(thing1 + ' is true');
+      document.getElementById("trueOrFalse").innerHTML = `${thing1} is truthy, ${thing2} is falsey.`;
+    } else {
+      console.log(thing2 + ' is true.')
+      document.getElementById("trueOrFalse").innerHTML = `${thing1} is falsey, ${thing2} is truthy.`;
+    }
+   }
+}
 
 
 // Write a JavaScript program that runs when both things are not true.  
-
+const neitherTrue = (thing1, thing2) => {
+  if (!thing1 && !thing2) {
+    console.log('neither is true');
+    document.getElementById("trueOrFalse").innerHTML = `Both ${thing1} and ${thing2} are falsey.`;
+  }
+}
 // ***************************
 //         PART TWO
 // ***************************
